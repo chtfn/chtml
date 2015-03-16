de25b264a388e1f823db0630601533af197d645c
Lochlan Morrissey <lochlan@pizpalu.net>
2015-03-16 15:24:35 +1000

    Basic template from https://docs.python.org/3/library/html.parser.html#module-html.parser.

--- chtml.py
+++ chtml.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
 from html.parser import HTMLParser
+
+class htmlparse(HTMLParser):
+    def starttag(self, tag, attrs):
+        pass
+    def endtag(self, tag):
+        pass
+    def contents(self, data):
+        pass
