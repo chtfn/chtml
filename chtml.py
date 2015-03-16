f7b0aa735fa63af2f449b0e955b87f1411438166
Lochlan Morrissey <lochlan@pizpalu.net>
2015-03-16 15:33:06 +1000

    Mock-up of parser output.

--- chtml.py
+++ chtml.py
@@ -3,10 +3,13 @@
 from html.parser import HTMLParser
 
 # Define the parser for vanilla html
-class vanhtml(HTMLParser):
-    def starttag(self, tag, attrs):
-        pass
-    def endtag(self, tag):
-        pass
-    def contents(self, data):
-        pass
+class html(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print('({0}'.format(tag),)
+    def handle_endtag(self, tag):
+        print(')')
+    def handle_data(self, data):
+        print(data)
+
+vanilla = vanhtml()
+vanilla.feed('<html><head><title>Test</title></head>')
